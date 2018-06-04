1223e843612f608bf21b7750a849ae408373f9cd
zokicpokic <zokicpokic@gmail.com>
2018-06-04 12:14:29 +0000

    added service

---
+++ CloudStorageHelper/AzureBlobService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var service = new Service1();
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine("Starting service...");
+                service.ConsoleStart(args);
+                Console.WriteLine("Service started. Press 'Enter' to stop!");
+                Console.ReadLine();
+                Console.WriteLine("Stopping service...");
+                service.ConsoleStop();
+            }
+            else
+            {
+                ServiceBase.Run(new ServiceBase[] { service });
+            }
+        }
+    }
+}
