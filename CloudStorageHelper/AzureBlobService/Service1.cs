e1eb86cd91f16924881dcafe80c7cd294d1070f0
zokicpokic <zokicpokic@gmail.com>
2018-06-04 16:21:25 +0000

    working with service

--- CloudStorageHelper/AzureBlobService/Service1.cs
+++ CloudStorageHelper/AzureBlobService/Service1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
@@ -17,8 +18,22 @@ namespace AzureBlobService
             InitializeComponent();
         }
 
+        private TransferManager _transferManager;
+
         protected override void OnStart(string[] args)
         {
+            Task transferManager = Task.Factory.StartNew(
+            () =>
+            {
+
+                _transferManager = new TransferManager(
+                    ConfigurationManager.AppSettings["accountName"],
+                    ConfigurationManager.AppSettings["accountKey"],
+                    ConfigurationManager.AppSettings["rootDir"]
+                );
+
+                _transferManager.Start();
+            });
         }
 
         protected override void OnStop()
---
+++ CloudStorageHelper/AzureBlobService/TransferManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureStorage;
+
+namespace AzureBlobService
+{
+    public class TransferManager
+    {
+        private string _accName = string.Empty;
+        private string _accKey = string.Empty;
+        private string _rootDir = string.Empty;
+        private int _autoRecoveryAttempts = 0;
+        private BlobManager _blobManager;
+
+
+        public TransferManager(string accName, string accKey, string rootDir, int autoRecoveryAttempts = 0)
+        {
+            _accName = accName ;
+            _accKey = accKey ;
+            _rootDir = rootDir ;
+            _autoRecoveryAttempts = autoRecoveryAttempts;
+            _blobManager = new BlobManager(_accName, _accKey, _autoRecoveryAttempts);
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
